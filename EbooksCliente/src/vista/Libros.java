f26773d9c39e17993616ee968dce6c10197b256e
1625690 <crystian.perafan@correounivalle.edu.co>
2017-05-15 20:49:22 -0500

    Agregado Cliente

---
+++ EbooksCliente/src/vista/Libros.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vista;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Familia
+ */
+public class Libros extends JInternalFrame{
+    
+}
